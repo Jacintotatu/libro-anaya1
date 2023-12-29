117a7ce3074448d0c8ef484d5739618c8f80f956
Jacintotatu <153659987+Jacintotatu@users.noreply.github.com>
2023-12-29 19:22:01 +0100

    Actualizar ejercicio_5-10.py

--- ejercicio_5-8.py
+++ ejercicio_5-8.py
@@ -1,1 +1,14 @@
-print("hello world")
+current_users = ['Manolo', 'Jacinto', 'David', 'Chicote', 'Pedro']
+
+new_users = ['antonio', 'juan', 'pedro', 'vicente', 'david', 'chicote']
+
+current_users_low = [user.lower() for user in current_users]#['manolo', 'jacinto', 'david', 'chicote', 'pedro']
+
+
+for i in new_users:
+
+    if i.lower() in current_users_low:
+        print(f'Tienes que introducir otro nombre, {i.title()} esta usado.')
+
+    else:
+        print('El nombre usuario esta disponible.')
